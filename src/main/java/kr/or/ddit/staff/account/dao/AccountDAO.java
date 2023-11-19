243b3af63183b6c054baf6a1dc7579e22a5184cf
Hyun <echo0913@naver.com>
2023-11-20 02:16:19 +0900

    ㄱㅈㅅㅅ

--- src/main/java/kr/or/ddit/staff/account/dao/AccountDAO.java
+++ src/main/java/kr/or/ddit/staff/account/dao/AccountDAO.java
@@ -1,5 +1,8 @@
 package kr.or.ddit.staff.account.dao;
 
-public class AccountDAO {
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AccountDAO {
 
 }
--- src/main/java/kr/or/ddit/staff/account/service/AccountService.java
+++ src/main/java/kr/or/ddit/staff/account/service/AccountService.java
@@ -1,5 +1,13 @@
 package kr.or.ddit.staff.account.service;
 
-public class AccountService {
+import java.util.List;
+import kr.or.ddit.staff.vo.StaffVO;
+import kr.or.ddit.student.vo.StudentVO;
+import kr.or.ddit.professor.vo.ProfessorVO;
 
+public interface AccountService {
+
+//	public boolean createStfAccount(StaffVO stf);
+//	public boolean createStdAcc(StudentVO std);
+//	public boolean createProAcc(ProfessorVO pro);
 }

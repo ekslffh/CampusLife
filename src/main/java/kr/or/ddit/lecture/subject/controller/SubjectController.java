bddc4703328e62687bbdb515663f738596f66eb6
Ohymin <hmin128600@gmail.com>
2023-11-20 15:48:01 +0900

    first commit

--- src/main/java/kr/or/ddit/lecture/subject/controller/SubjectController.java
+++ src/main/java/kr/or/ddit/lecture/subject/controller/SubjectController.java
@@ -1,5 +1,18 @@
 package kr.or.ddit.lecture.subject.controller;
 
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.or.ddit.lecture.subject.service.SubjectService;
+
+@Controller
+@RequestMapping("/subject/")
 public class SubjectController {
+
+	@Inject
+	private SubjectService service;
+	
 
 }

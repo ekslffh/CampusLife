2c1d47efecc79026725186d0e861bdac38f26216
Ohyemin <hmin128600@gmail.com>
2023-11-27 20:23:07 +0900

    과목 및 필수 과목 등록

--- src/main/java/kr/or/ddit/lecture/subject/controller/SubjectController.java
+++ src/main/java/kr/or/ddit/lecture/subject/controller/SubjectController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import kr.or.ddit.lecture.subject.service.SubjectService;
+import kr.or.ddit.lecture.subject.vo.SubjectDTO;
+import kr.or.ddit.lecture.subject.vo.SubjectEssentialVO;
 import kr.or.ddit.lecture.subject.vo.SubjectVO;
 
 @Controller
@@ -24,8 +26,8 @@ public class SubjectController {
 	private SubjectService service;
 
 	@ModelAttribute("subject")
-	public SubjectVO getSubject() {
-		return new SubjectVO();
+	public SubjectDTO getSubject() {
+		return new SubjectDTO();
 	}
 
 	@GetMapping("subjectList.do")
@@ -38,24 +40,93 @@ public class SubjectController {
 	}
 
 	@PostMapping("insertSubject.do")
-	public String insertSubject(@ModelAttribute("subject") SubjectVO sub, Errors errors, Model model) {
+	public String insertSubject(@ModelAttribute("subject") SubjectDTO sub, Errors errors, Model model) {
 
 		String viewName = null;
-		
-	      if(!errors.hasErrors()) {
-	    	  boolean subInsert = service.create(sub);
-		         
-		         if(subInsert) {
-		            viewName = "redirect:/professor/subject/subjectList.do";
-		         } else {
-		            model.addAttribute("message", "과목 개설 중 문제 발생");
-		            viewName = "professor/subject/subjectList";
-		         }
-		      } else {
-		         viewName = "professor/subject/subjectList";
-		      }
+		SubjectVO subVo = new SubjectVO();
+		SubjectEssentialVO subEvo = new SubjectEssentialVO();
 
-		return viewName;
+		if (!errors.hasErrors()) {
+	        // 필수 테이블에 저장
+	        if (sub.getSubRequired().equals("1")) {
+	        	// 과목 테이블에 저장
+	        	subVo.setSubNo(sub.getSubNo());
+	        	subVo.setSubNm(sub.getSubNm());
+	        	subVo.setSubMajCd(sub.getSubMajCd());
+	        	subVo.setSubStatus(sub.getSubStatus());
+	        	subVo.setSubType(sub.getSubType());
+	        	
+	        	service.create(subVo);
+	        	String seSubNo = subVo.getSubNo();
+	        	
+	            // seSubNo를 null로 설정하고 필수 테이블에 저장
+	            subEvo.setSeSubNo(seSubNo);
+	            subEvo.setSeMajCd(sub.getSubMajCd());
+	            subEvo.setSeGrade(sub.getSeGrade());
+
+	            boolean subVOInsert = service.insertSubjectEss(subEvo);
+
+
+	            if (subVOInsert) {
+	                viewName = "redirect:/professor/subject/subjectList.do";
+	            } else {
+	                model.addAttribute("message", "과목 개설 중 문제 발생");
+	                viewName = "professor/subject/subjectList";
+	            }
+	        } else {
+	            // 필수가 아닌 경우 과목 테이블에만 저장
+	            subVo.setSubNo(sub.getSubNo()); // 시퀀스에 의해 자동 생성
+	            subVo.setSubNm(sub.getSubNm());
+	            subVo.setSubMajCd(sub.getSubMajCd());
+	            subVo.setSubStatus(sub.getSubStatus());
+	            subVo.setSubType(sub.getSubType());
+
+	            boolean subVOInsert = service.create(subVo);
+
+	            if (subVOInsert) {
+	                viewName = "redirect:/professor/subject/subjectList.do";
+	            } else {
+	                model.addAttribute("message", "과목 개설 중 문제 발생");
+	                viewName = "professor/subject/subjectList";
+	            }
+	        }
+	    } else {
+	        viewName = "professor/subject/subjectList";
+	    }
+
+	    return viewName;
+		
+//		if (!errors.hasErrors()) {
+//			
+//			subVo.setSubNo(sub.getSubNo());
+//			subVo.setSubNm(sub.getSubNm());
+//			subVo.setSubMajCd(sub.getSubMajCd());
+//			subVo.setSubStatus(sub.getSubStatus());
+//			subVo.setSubType(sub.getSubType());
+//			
+//			boolean subVOInsert = service.create(subVo);
+//
+//			if (sub.getSubRequired().equals("1")) {
+//				subEvo.setSeSubNo(sub.getSubNo());
+//				subEvo.setSeMajCd(sub.getSubMajCd());
+//				subEvo.setSeGrade(sub.getSeGrade());
+//				
+//				service.insertSubjectEss(subEvo);
+//				
+//			}
+//			
+//			
+//			if (subVOInsert) {
+//				viewName = "redirect:/professor/subject/subjectList.do";
+//			} else {
+//				model.addAttribute("message", "과목 개설 중 문제 발생");
+//				viewName = "professor/subject/subjectList";
+//			}
+//		} else {
+//			viewName = "professor/subject/subjectList";
+//		}
+//
+//		return viewName;
 	}
 
 }
--- src/main/java/kr/or/ddit/lecture/subject/service/SubjectService.java
+++ src/main/java/kr/or/ddit/lecture/subject/service/SubjectService.java
@@ -1,9 +1,10 @@
 package kr.or.ddit.lecture.subject.service;
 
 import kr.or.ddit.common.service.ServiceIfs;
+import kr.or.ddit.lecture.subject.vo.SubjectEssentialVO;
 import kr.or.ddit.lecture.subject.vo.SubjectVO;
 
 public interface SubjectService extends ServiceIfs<SubjectVO> {
 	
-	
+	public boolean insertSubjectEss(SubjectEssentialVO subEvo);
 }
